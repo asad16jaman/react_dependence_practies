e99b46c07c074ca8b5e8a4beb58e2f4676954587
asad <asaduzzaman240250@gmail.com>
2021-04-06 19:56:11 +0600

    inisialize the project

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import App from './App'
+// import './boot/bootstrap.min.css'
+import ReactDOM from 'react-dom'
+// import './boot/bootstrap.min.css'
+
+ReactDOM.render(<App />,document.getElementById())
